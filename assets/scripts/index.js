f5b206ab19f09ebae3226c57d191c06675c7a258
ChaviraAlex <112144506+ChaviraAlex@users.noreply.github.com>
2022-08-30 22:43:09 -0500

    calculator

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,2 +1,40 @@
-const backdropElement = document.getElementById('backdrop');
-const modalLinkElements = document.querySelectorAll('.info-modal');
+const addbtn = document.getElementById('add');
+const substractbtn = document.getElementById('substract');
+const multiplybtn = document.getElementById('multiply');
+const divisionbtn = document.getElementById('division');
+
+
+let result;
+
+function add(num1,num2){
+    num1 = parseInt(document.getElementById('number1').value);
+    num2 = parseInt(document.getElementById('number2').value);
+    result = num1 + num2;
+    document.getElementById('result').innerHTML = result;
+}
+
+function substract(num1,num2){
+    num1 = parseInt(document.getElementById('number1').value);
+    num2 = parseInt(document.getElementById('number2').value);
+    result = num1 - num2;
+    document.getElementById('result').innerHTML = result;
+}
+
+function multiply(num1,num2){
+    num1 = parseInt(document.getElementById('number1').value);
+    num2 = parseInt(document.getElementById('number2').value);
+    result = num1 * num2;
+    document.getElementById('result').innerHTML = result;
+}
+
+function division(num1,num2){
+    num1 = parseInt(document.getElementById('number1').value);
+    num2 = parseInt(document.getElementById('number2').value);
+    result = num1 / num2;
+    document.getElementById('result').innerHTML = result;
+}
+
+addbtn.addEventListener('click',add);
+substractbtn.addEventListener('click',substract);
+multiplybtn.addEventListener('click',multiply);
+divisionbtn.addEventListener('click',division);
